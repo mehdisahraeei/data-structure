61b13388f3c27073baf10aefacc15fdd1b4435b1
mahdisahraei <mahdi_sahrai@yahoo.com>
2022-12-06 12:56:02 +0330

    one project was done about array in Linear structures

---
+++ Linear structures/01-array/Array.kt
@@ -0,0 +1,9 @@
+class Array {
+
+ 
+    fun Mehtod1(): IntArray {
+        val arr = intArrayOf(5,6,7,3)
+        return arr
+    }
+
+}
---
+++ Linear structures/01-array/main.kt
@@ -0,0 +1,17 @@
+
+
+
+fun main(args: Array) {
+
+
+
+        val  obj = Array()
+        print(obj.Mehtod1().get(3).toString())
+        //Log.i("log", obj.Mehtod1().get(3).toString())
+
+}
+
+
+
+//print or log output
+//2022-12-06 12:50:38.462  3513-3513  log:           3
