45b7fed7f13bd4ca92d9a3230b8fe02f3e5aae02
mehdisahraeei <mahdi_sahrai@yahoo.com>
2023-07-26 19:09:45 +0330

    The previous codes were commented

--- blockchain/01-practice-weak-blockchain/weak-blockchain.py
+++ blockchain/01-practice-weak-blockchain/weak-blockchain.py
@@ -2,54 +2,43 @@ import hashlib
 import time
 import json
 
-
 class BlockChain:
-    
     def __init__(self):
-        self.hashs = []
-        self.chain = []
+        self.hashs = []     # List to store the hashes of blocks
+        self.chain = []     # List to store the blocks
     
-    
-    def create_block(self, user_id, coin_name, value_trx,timestamp):
+    def create_block(self, user_id, coin_name, value_trx, timestamp):
         if len(self.hashs) == 0:
-             print('you should do hash first block')
+            print('you should do hash first block')
         else:
-            
-            new_block = {'previous hash':self.hashs[-1] ,'user id':user_id,
-                           'coin name':coin_name,'value trx':value_trx,'timestamp':timestamp}
-            self.chain.append(new_block)  
-#             return new_block
+            new_block = {'previous hash': self.hashs[-1], 'user id': user_id,
+                         'coin name': coin_name, 'value trx': value_trx, 'timestamp': timestamp}
+            self.chain.append(new_block)  # Add the new block to the chain
+            # return new_block
     
-    
-    
-    def add_block(self, user_id, coin_name, value_trx,timestamp):
-        default_block = {'user id':user_id,
-                       'coin name':coin_name,'value trx':value_trx,'timestamp':timestamp}
-        self.chain.append(default_block)
-        
+    def add_block(self, user_id, coin_name, value_trx, timestamp):
+        default_block = {'user id': user_id, 'coin name': coin_name,
+                         'value trx': value_trx, 'timestamp': timestamp}
+        self.chain.append(default_block)  # Add the default block to the chain
         return default_block
-    
     
     def hash_block(self, whichblock):
         if whichblock == 'def':  # Default block
-            to_str_ = json.dumps(self.chain[0])
-            first_hash = hashlib.sha256(to_str_.encode()).hexdigest()
-            self.hashs.append([first_hash])
+            to_str_ = json.dumps(self.chain[0])  # Convert the block to a JSON string
+            first_hash = hashlib.sha256(to_str_.encode()).hexdigest()  # Calculate the SHA-256 hash of the JSON string
+            self.hashs.append([first_hash])  # Add the hash to the list of hashes
             return first_hash
         
-        elif whichblock == 'next':  # Second block
-            to_str_ = json.dumps(self.chain[-1])
-            next_hash = hashlib.sha256(to_str_.encode()).hexdigest()
-            self.hashs.append([next_hash])
+        elif whichblock == 'next':  # Second block and subsequent blocks
+            to_str_ = json.dumps(self.chain[-1])  # Convert the last block to a JSON string
+            next_hash = hashlib.sha256(to_str_.encode()).hexdigest()  # Calculate the SHA-256 hash of the JSON string
+            self.hashs.append([next_hash])  # Add the hash to the list of hashes
             return next_hash
         
         else:
             return None
     
-    
-    
     def last_block(self):
-        x = self.chain[-1]
+        x = self.chain[-1]  # Get the last block from the chain
         return x
-    
-    
+
--- blockchain/02-practice-better-blockchain/better-blockchain.py
+++ blockchain/02-practice-better-blockchain/better-blockchain.py
@@ -5,39 +5,38 @@ import time
 class BlockChain:
     
     def __init__(self):
-        self.chain = []
-    
+        self.chain = []  # Initialize an empty list to store blockchain blocks.
     
     def creating_new_block(self, user_id, coin_name, value_trx):
-        if not self.chain: # if len(self.chain) == 0: 
+        if not self.chain:  # Check if the chain is empty (no blocks created yet).
             print('You should create the first block and do hash default chain ')
         else:
             new_block = {
-                'previous_hash': self.hash_block(self.chain[-1]),
+                'previous_hash': self.hash_block(self.chain[-1]),  # Get the hash of the previous block.
                 'user_id': user_id,
                 'coin_name': coin_name,
                 'value_trx': value_trx,
-                'timestamp': time.time()
+                'timestamp': time.time()  # Add the current timestamp to the block.
             }
-            self.chain.append(new_block)
+            self.chain.append(new_block)  # Append the new block to the blockchain.
             return new_block
     
-    
     def adding_first_block(self, user_id, coin_name, value_trx):
-        
-        default_block = {'user_id': user_id,'coin_name': coin_name,
-                             'value_trx': value_trx,'timestamp': time.time()}    
-        self.chain.append(default_block)
+        default_block = {
+            'user_id': user_id,
+            'coin_name': coin_name,
+            'value_trx': value_trx,
+            'timestamp': time.time()  # Add the current timestamp to the block.
+        }
+        self.chain.append(default_block)  # Append the first block to the blockchain.
         return default_block
     
-    
-    def hash_block(self,block):
-        to_str_ = json.dumps(block, sort_keys=True)
-        return hashlib.sha256(to_str_.encode()).hexdigest()
-    
+    def hash_block(self, block):
+        to_str_ = json.dumps(block, sort_keys=True)  # Convert the block dictionary to a JSON string.
+        return hashlib.sha256(to_str_.encode()).hexdigest()  # Return the SHA-256 hash of the JSON string.
     
     def last_block(self):
-        if not self.chain:
+        if not self.chain:  # Check if the chain is empty (no blocks created yet).
             return None
-        return self.chain[-1]
+        return self.chain[-1]  # Return the last block in the blockchain.
 
